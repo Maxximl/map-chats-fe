070a0eaedb21e33351d6cd9ba565cdce292f86d1
Maxim Lavrinenko <max1mlavrinenko@yandex.ruu>
2025-05-26 10:11:32 +0500

    add eruda

--- src/main.tsx
+++ src/main.tsx
@@ -4,3 +4,7 @@ import './index.css'
 import AppConfig from '@app/AppConfig.tsx'
 
 createRoot(document.getElementById('root')!).render(<AppConfig />)
+
+// if (import.meta.env.MODE === 'development') {
+import('./eruda')
+// }
